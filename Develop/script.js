66b8995cebb32cd3f6c150fb1e282db4955dda60
billhamilton68 <billhamilton68@gmail.com>
2023-03-13 02:13:51 -0700

    tested_updates

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,10 @@
-  // Assignment Code
-  // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var psswrdTxtarea = document.getElementById('password');
-var lCaseOpt = 'abcdefghijklmnopqrstuvwxyz';
-var uCaseOpt = lCaseOpt.toUpperCase;
-var spclChars = "#@%*$+"
-var numChoices = "0123456789"
+// Assignment Code
+const generateBtn = document.querySelector("#generate");
+const psswrdTxtArea = document.getElementById("password");
+const lCaseOpt = "abcdefghijklmnopqrstuvwxyz";
+const uCaseOpt = lCaseOpt.toUpperCase();
+const spclChars = "#@%*$+"
+const numChoices = "0123456789"
 // create and initialize variables for password random lettters charecters 
 
 // Write password to the #password input
@@ -30,28 +29,30 @@ function generatePassword() {
   while (isNaN(psswrdLength) || psswrdLength < 8 || psswrdLength > 128) {
 
     psswrdLength = parseInt(prompt("Your password is invalid, it must be between 8 and 128 chrecters!!!"));
+
+    // Confirm a number between 8 and 128 is entered
   }
 
   var lCaseChoose = confirm("Do want to use lowercase letters?");
   var uCaseChoose = confirm("Do you want to use uppercase letters?");
   var numChoose = confirm("Do you want numbers?");
-  var spclCharsChoose = confirm("Do you want special charecters?");
+  var spclCharsChoose = confirm("Do you want special characters?");
 
-  
+  //prompts users to choose whether there password will include uppercase letters, lowercase letters, numbers and special characters
 
   while (!lCaseChoose && !uCaseChoose && !numChoose && !spclCharsChoose) {
-    
-    alert("You must include at minimum one charecter type!!!");
-   var lCaseChoose = confirm("Do want to use lowercase letters?");
-   var uCaseChoose = confirm("Do you want to use uppercase letters?");
-   var numChoose = confirm("Do you want numbers?");
-   var spclCharsChoose = confirm("Do you want special charecters?");
 
- 
+    alert("You must include at minimum one charecter type!!!");
+    var lCaseChoose = confirm("Do want to use lowercase letters?");
+    var uCaseChoose = confirm("Do you want to use uppercase letters?");
+    var numChoose = confirm("Do you want numbers?");
+    var spclCharsChoose = confirm("Do you want special charecters?");
+    //enforces requirement at least one charecter before moving on to the password detail prompts
+
   }
 
-var charSet = '';
-  
+  var charSet = "";
+
   if (lCaseChoose) {
     charSet += lCaseOpt;
   }
@@ -64,12 +65,13 @@ var charSet = '';
   if (spclCharsChoose) {
     charSet += spclChars;
   }
-
+  //add users password charectrer choices to var charecterset
   // Generate password
   for (let i = 0; i < psswrdLength; i++) {
     password += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
+  // loops through charSet variable to create random values based on the users choices for each iteration
+  return password;
 
-  // Update password textarea with generated password
-  psswrdTxtarea.value = password;
+
 }
